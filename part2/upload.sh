88d16b1b175c47300945b57e945c8f13569317ea
chiroptera <chiroptera@dove.(none)>
2013-12-17 09:55:05 +0000

    sh

---
+++ part2/upload.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#cd ~/Transferências/part2
+
+
+scp bin/prog inits/shiftBgroup3w1.sh TBG3@192.168.27.202:~/
+scp bin/prog inits/shiftBgroup3w2.sh TBG3@192.168.27.204:~/
+scp bin/prog inits/shiftBgroup3w3.sh TBG3@192.168.27.203:~/
+scp bin/prog inits/shiftBgroup3w4.sh TBG3@192.168.27.206:~/
+scp bin/prog inits/shiftBgroup3w5.sh TBG3@192.168.27.205:~/
+scp bin/prog inits/shiftBgroup3w6.sh TBG3@192.168.27.207:~/
+scp bin/prog inits/shiftBgroup3w7.sh TBG3@192.168.27.208:~/
+scp bin/prog inits/shiftBgroup3w8.sh TBG3@192.168.27.209:~/
+
+scp bin/prog inits/"shiftBgroup3w1 (cópia).sh" TBG3@192.168.27.202:~/
+scp bin/prog inits/"shiftBgroup3w2 (cópia).sh" TBG3@192.168.27.204:~/
+scp bin/prog inits/"shiftBgroup3w3 (cópia).sh" TBG3@192.168.27.203:~/
+scp bin/prog inits/"shiftBgroup3w4 (cópia).sh" TBG3@192.168.27.206:~/
+scp bin/prog inits/"shiftBgroup3w5 (cópia).sh" TBG3@192.168.27.205:~/
+scp bin/prog inits/"shiftBgroup3w6 (cópia).sh" TBG3@192.168.27.207:~/
+scp bin/prog inits/"shiftBgroup3w7 (cópia).sh" TBG3@192.168.27.208:~/
+scp bin/prog inits/"shiftBgroup3w8 (cópia).sh" TBG3@192.168.27.209:~/
+
+echo "aiFoh4fu";ssh TBG3@192.168.27.203
+aiFoh4fu
